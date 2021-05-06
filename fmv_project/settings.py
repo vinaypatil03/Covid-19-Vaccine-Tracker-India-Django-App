a54545ec82cd10c7cc62891cefb0daf832783dc5
vinaypatil <vpatilvinu@gmail.com>
2021-05-06 13:02:20 +0530

    initial commit

--- fmv_project/settings.py
+++ fmv_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = "django-insecure-@o0f_0^jbqwsanl(7e@5d!=7)@=btc$xp&6&6#sh*9u7(+!$pu"
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
